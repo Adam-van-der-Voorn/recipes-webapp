8dd0f758a5e88b62f16bc9754df2848726967c10
Adam van der Voorn <adamvdvoorn@gmail.com>
2022-03-12 10:58:55 +1300

    recipie types modified

--- src/types/recipieTypes.ts
+++ src/types/recipieTypes.ts
@@ -1,96 +1,40 @@
 
-export type Ingredient = {
-    index: number;
-    name: string; // no duplicates
-    quantity: {
-        value: number;
-        unit: string;
-    }
-    kjs?: number;
+export type UnitVal = {
+  value: number;
+  unit: string;
 }
 
-export type Substitution = {
-    changes: SubstitutionPart[];
+export type Ingredient = {
+  index: number;
+  name: string; // no duplicates
+  quantity: UnitVal;
+  kjs?: number;
 }
 
 export type SubstitutionPart = {
-    ingredientName: string;
-    action: 'add' | 'remove'
-    amount: number;
+  ingredientName: string;
+  action: 'add' | 'remove'
+  amount: number;
+}
+
+export type Substitution = {
+  changes: SubstitutionPart[];
+}
+
+export type Ingredients = {
+  list: Ingredient[],
+  anchor?: string;
 }
 
 export type Recipie = {
-    name: string;
-    coverPhoto?: any;
-    timeframe?: string;
-    servings?: {
-        value: number;
-        unit: string;
-    }
-    ingredients: {
-        list: Ingredient[],
-        anchor?: string;
-    }
-    instructions?: string;
-    substitutions?: Substitution[];
+  name: string;
+  timeframe?: string;
+  servings?: UnitVal;
+  ingredients: Ingredients;
+  instructions?: string;
+  substitutions?: Substitution[];
 }
 
 export type RecipieSet = {
-    recipies: Recipie[];
+  recipies: Recipie[];
 }
-
-export const dummyData: RecipieSet = {
-    recipies: [
-      {
-        name: "toast",
-        ingredients: {
-          list: [
-            {
-              index: 0,
-              name: "bread",
-              quantity: {
-                value: 1,
-                unit: 'slice'
-              }
-            }
-          ]
-        }
-      },
-      {
-        name: "Ham",
-        ingredients: {
-          list: []
-        }
-      },
-      {
-        name: "Bread",
-        ingredients: {
-          list: []
-        }
-      },
-      {
-        name: "Steak",
-        ingredients: {
-          list: []
-        }
-      },
-      {
-        name: "Lamb",
-        ingredients: {
-          list: []
-        }
-      },
-      {
-        name: "Hummus",
-        ingredients: {
-          list: []
-        }
-      },
-      {
-        name: "Gravy",
-        ingredients: {
-          list: []
-        }
-      },
-    ]
-  }
