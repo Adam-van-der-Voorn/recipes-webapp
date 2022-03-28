b2bbb5f4c29b7acd8b03134476903cf5a90e18b9
Adam van der Voorn <adamvdvoorn@gmail.com>
2022-03-28 22:34:28 +1300

    service worker cache - still has known issues

--- src/index.tsx
+++ src/index.tsx
@@ -3,27 +3,37 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import './palette.css'
-import './style.css'
+import './palette.css';
+import './style.css';
 import AddRecipiePage from './components/routes/add-recipie/AddRecipiePage';
 import RecipieSetPage from './components/routes/home/RecipieSetPage';
 import ViewRecipiePage from './components/routes/view-recipie/ViewRecipiePage';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<RecipieSetPage />} />
-          <Route path="add-recipie" element={<AddRecipiePage />} />
-          <Route path=":recipieName" element={<ViewRecipiePage />} />
-        </Route>
-        <Route path="*" element={<div>BAD PATH !!!!</div>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(function (registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function (error) {
+            console.log('Service worker registration failed, error:', error);
+        });
+}
 
-  document.getElementById('root')
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route index element={<RecipieSetPage />} />
+                    <Route path="add-recipie" element={<AddRecipiePage />} />
+                    <Route path=":recipieName" element={<ViewRecipiePage />} />
+                </Route>
+                <Route path="*" element={<div>BAD PATH !!!!</div>} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
