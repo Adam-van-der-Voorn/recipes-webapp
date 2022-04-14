4690552c2491b88b946c1b945185609029db78c2
Adam van der Voorn <adamvdvoorn@gmail.com>
2022-04-15 06:52:10 +1200

    substitutions input fleshed out

--- src/components/recipie-form/SubstitutionAdditionsField.tsx
+++ src/components/recipie-form/SubstitutionAdditionsField.tsx
@@ -1,4 +1,4 @@
-import { FieldArrayRenderProps, useFormikContext } from "formik";
+import { Field, FieldArrayRenderProps, useFormikContext } from "formik";
 import { useEffect } from "react";
 import { RecipieFormData } from "./RecipieForm";
 
@@ -10,16 +10,25 @@ type Props = {
 function SubstitutionAdditionsField({ index, arrayHelpers }: Props) {
     const { values } = useFormikContext<RecipieFormData>();
 
+    useEffect(() => {
+        if (values.substitutions[index].additions.length === 0) {
+            arrayHelpers.push({ amount: '', ingredientName: '' });
+        }
+    }, [])
+
     return (
         <div>
             <div>with...</div>
             {
                 values.substitutions[index].additions.map((addition, idx) => (
                     <div key={idx}>
-                        <button type="button" onClick={() => arrayHelpers.remove(idx)}>
-                            --
-                        </button>
-                        y
+                        {   idx !== 0 && 
+                            <button type="button" onClick={() => arrayHelpers.remove(idx)}>
+                                --
+                            </button>
+                        }
+                        <Field name={`substitutions.${index}.additions.${idx}.quantity`} type="text" /> of
+                        <Field name={`substitutions.${index}.additions.${idx}.ingredientName`} type="text" />
                     </div>
                 ))
             }
--- src/components/recipie-form/SubstitutionRemovalsField.tsx
+++ src/components/recipie-form/SubstitutionRemovalsField.tsx
@@ -1,4 +1,4 @@
-import { FieldArrayRenderProps, useFormikContext } from "formik";
+import { Field, FieldArrayRenderProps, useFormikContext } from "formik";
 import { useEffect } from "react";
 import { RecipieFormData } from "./RecipieForm";
 
@@ -10,20 +10,36 @@ type Props = {
 function SubstitutionRemovalsField({ index, arrayHelpers }: Props) {
     const { values } = useFormikContext<RecipieFormData>();
 
+    useEffect(() => {
+        if (values.substitutions[index].removals.length === 0) {
+            arrayHelpers.push({ amount: '', ingredientName: '' });
+        }
+    }, []);
+
     return (
         <div>
             <div>replace...</div>
             {
                 values.substitutions[index].removals.map((removal, idx) => (
                     <div key={idx}>
-                        <button type="button" onClick={() => arrayHelpers.remove(idx)}>
-                            --
-                        </button>
-                        x
+                        {idx !== 0 &&
+                            <button type="button" onClick={() => arrayHelpers.remove(idx)}>
+                                --
+                            </button>
+                        }
+                        <Field name={`substitutions.${index}.removals.${idx}.quantity`} type="text" /> of
+                        <Field as="select" name={`substitutions.${index}.removals.${idx}.ingredientName`} >
+                            {
+                                values.ingredients.list.map(ingredient => {
+                                    return <option value={ingredient.name.toLowerCase()}>{ingredient.name}</option>;
+                                })
+                            }
+                        </Field>
+                        <br />
                     </div>
                 ))
             }
-            <button type="button" onClick={() => arrayHelpers.push({ amount: '', ingredientName: ''})}>
+            <button type="button" onClick={() => arrayHelpers.push({ amount: '', ingredientName: '' })}>
                 ++
             </button >
         </div >
