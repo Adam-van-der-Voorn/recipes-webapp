08ee8c65dfbb455a6e54278dc0b101d51a3746f1
Adam van der Voorn <adamvdvoorn@gmail.com>
2022-05-06 22:22:26 +1200

    refactored firbase setup into own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import React, { createContext, useEffect, useRef, useState } from "react";
+import React, { createContext } from "react";
 import { Outlet } from "react-router-dom";
 import { Recipie } from "./types/recipieTypes";
-import { initializeApp } from "firebase/app";
-import { collection, deleteDoc, doc, enableIndexedDbPersistence, getFirestore, onSnapshot, setDoc } from "firebase/firestore";
+import { deleteDoc, doc } from "firebase/firestore";
 import useRecipieStorage from "./util/hooks/useRecipieStorage";
+import setupFirebase from "./util/setupFirestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBZaU6DaCaZHqeHFuQwg8enna3LkEkJNu8",
-    authDomain: "recipiesapp-85118.firebaseapp.com",
-    projectId: "recipiesapp-85118",
-    storageBucket: "recipiesapp-85118.appspot.com",
-    messagingSenderId: "192864545623",
-    appId: "1:192864545623:web:8178b8ffa7b2b8b43d6c12",
-    measurementId: "G-822KJ4KS41"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-enableIndexedDbPersistence(db)
-    .catch((err) => {
-        if (err.code === 'failed-precondition') {
-            console.error("FireStore IndexedDB - failed precondition: Multiple tabs open, persistence can only be enabled in one tab at a a time.");
-        }
-        else if (err.code === 'unimplemented') {
-            console.error("FireStore IndexedDB: The current browser does not support all of the features required to enable persistence");
-        }
-    });
+const { db } = setupFirebase()
 
 const del = (collectionName: string, docName: string) => {
     deleteDoc(doc(db, collectionName, docName))
---
+++ src/util/setupFirestore.ts
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
+
+export default function setupFirebase() {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyBZaU6DaCaZHqeHFuQwg8enna3LkEkJNu8",
+        authDomain: "recipiesapp-85118.firebaseapp.com",
+        projectId: "recipiesapp-85118",
+        storageBucket: "recipiesapp-85118.appspot.com",
+        messagingSenderId: "192864545623",
+        appId: "1:192864545623:web:8178b8ffa7b2b8b43d6c12",
+        measurementId: "G-822KJ4KS41"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    enableIndexedDbPersistence(db)
+        .catch((err) => {
+            if (err.code === 'failed-precondition') {
+                console.error("FireStore IndexedDB - failed precondition: Multiple tabs open, persistence can only be enabled in one tab at a a time.");
+            }
+            else if (err.code === 'unimplemented') {
+                console.error("FireStore IndexedDB: The current browser does not support all of the features required to enable persistence");
+            }
+        });
+    return { firebaseApp: app, db: db };
+}
